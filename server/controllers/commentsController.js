5389ab797c25dd17cf28b024b4323d9f68c7d8ce
Jack Sprinkle <sprinkle.jas@gmail.com>
2022-12-06 12:53:37 -0500

    edit comments controller to resp with user name instead of id

--- server/controllers/commentsController.js
+++ server/controllers/commentsController.js
@@ -4,8 +4,14 @@ const {uuid} = require('uuidv4');
 exports.getComments = async(req, resp) => {
     try {
         const comments = await knex('comments')
-            .select('content', 'created_on', 'user_id', 'id')
+            .join('users', 'comments.user_id', '=', 'users.id')
             .where({post_id: req.params.postId})
+            .select(
+                'comments.id',
+                'comments.content', 
+                'comments.created_on', 
+                'users.user_name'
+            )
         resp.status(200).json(comments)
     } catch (error) {
         resp.status(400).send(`Error retreiving Comments for ${req.params.postId} ${error}`)
