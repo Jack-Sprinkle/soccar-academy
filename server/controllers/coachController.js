8689039d75e0bc16c19516e1b5af7d8a8cfdc3c5
Jack Sprinkle <sprinkle.jas@gmail.com>
2022-12-06 12:42:02 -0500

    adjust coach controller to respond correctly

--- server/controllers/coachController.js
+++ server/controllers/coachController.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 exports.getCoaches = async(req, resp) => {
     try {
         const coachData = await knex('users')
-            .select('user_name', 'discord_name', 'mmr_standard', 'user_bio', 'user_coach');
+            .select('user_name', 'discord_name', 'mmr_standard', 'user_bio', 'user_coach')
+            .where({user_coach: 'yes'});
         resp.status(200).json(coachData)
     } catch (error) {
         resp.status(400).send(`Error retreiving coaches ${error}`)
