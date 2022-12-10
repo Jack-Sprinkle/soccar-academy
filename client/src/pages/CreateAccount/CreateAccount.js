218eee076ebf612dd6e82d0fe695f00361ac8d81
Jack Sprinkle <sprinkle.jas@gmail.com>
2022-12-09 19:01:04 -0500

    set up form validation schema

--- client/src/pages/CreateAccount/CreateAccount.js
+++ client/src/pages/CreateAccount/CreateAccount.js
@@ -1,4 +1,19 @@
 import './CreateAccount.scss';
+import {Formik, Form, Field} from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+//Set up form validation schema
+const newAccountSchema = Yup.object().shape({
+    username: Yup.string().min(2, 'Too Short').max(50, 'Too Long').required('This field is required'),
+    email: Yup.string().email('Invalid email').required('This field is required.'),
+    epicId: Yup.string().min(2, 'Too short').max(50, 'Too long').required('This field is required'),
+    discordId: Yup.string().min(2, 'Too short').max(50, 'Too long').required('This field is required'),
+    standardMMR: Yup.number().required('This field is required'),
+    bio: Yup.string(),
+    password: Yup.string().required('This field is required'),
+    coach: Yup.string().required()
+});
 
 function CreateAccount() {
     return (
